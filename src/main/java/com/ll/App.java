15e9546e0bf3c36ae303d736c152a5aea90445eb
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-11-06 17:24:27 +0900

    step3, 4

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,6 +6,7 @@ public class App {
     public static void run() {
         System.out.println("== 명언 앱 ==");
         String cmd;
+        int cnt = 0;
 
         while (true){
             System.out.print("명령) ");
@@ -17,10 +18,14 @@ public class App {
                     return;   // 명령: "종료"일 때 앱 종료
 
                 case "등록":
+                    cnt=++cnt;
+
                     System.out.print("명언 : ");
                     String content = scanner.nextLine();
                     System.out.print("작가 : ");
                     String author = scanner.nextLine();
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", cnt);
             }
         }
     }
