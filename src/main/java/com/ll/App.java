627c8541876532c6b69f83c3d9c645922681647a
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-11-06 16:27:50 +0900

    step2

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -10,11 +10,17 @@ public class App {
         while (true){
             System.out.print("명령) ");
             Scanner scanner = new Scanner(System.in);
-            cmd = scanner.next();      // 명령 입력 받기
+            cmd = scanner.nextLine();      // 명령 입력 받기
 
             switch (cmd){
                 case "종료":
                     return;   // 명령: "종료"일 때 앱 종료
+
+                case "등록":
+                    System.out.print("명언 : ");
+                    String content = scanner.nextLine();
+                    System.out.print("작가 : ");
+                    String author = scanner.nextLine();
             }
         }
     }
