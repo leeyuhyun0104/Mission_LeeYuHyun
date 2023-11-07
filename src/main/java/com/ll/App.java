0d693cb90d7b70c56dcc90c4054d7f7b42afd6cc
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-11-07 16:58:14 +0900

    method refactoring & step5

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,31 +1,68 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
+
+    static String cmd;
+    static int cnt = 0;    // 등록된 명언의 개수, 0부터 시작
+    static List<Quotation> qList = new ArrayList<>();   // 명언 저장 리스트
     public static void run() {
         System.out.println("== 명언 앱 ==");
-        String cmd;
-        int cnt = 0;
 
-        while (true){
+        while (true) {
             System.out.print("명령) ");
             Scanner scanner = new Scanner(System.in);
             cmd = scanner.nextLine();      // 명령 입력 받기
 
-            switch (cmd){
+            switch (cmd) {
                 case "종료":
                     return;   // 명령: "종료"일 때 앱 종료
 
                 case "등록":
-                    cnt=++cnt;
+                    create();     // 명령: "등록"일 때 create() 메소드 실행
+                    break;
 
-                    System.out.print("명언 : ");
-                    String content = scanner.nextLine();
-                    System.out.print("작가 : ");
-                    String author = scanner.nextLine();
+                case "목록":
+                    read();    // 명령: "목록"일 때 read() 메소드 실행
+                    break;
+            }
+        }
+    }
 
-                    System.out.printf("%d번 명언이 등록되었습니다.\n", cnt);
+    // 명언 등록 메소드
+    static void create(){
+
+        System.out.print("명언 : ");
+        Scanner scanner = new Scanner(System.in);
+        String content = scanner.nextLine();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();   // scanner로 명언, 작가 입력 받음
+
+        cnt++;  // 등록된 명언의 개수 1씩 증가
+        int id = cnt;
+
+        Quotation q = new Quotation(id, content, author);
+        qList.add(q);    // 리스트에 명언 객체 추가
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+    }
+
+    // 명언 목록 출력 메소드
+    static void read(){
+
+        if(qList.isEmpty())
+            System.out.println("등록된 명언이 없습니다.");
+
+        else {
+            System.out.println("번호 / 작가 / 명언");
+            System.out.println("----------------------");
+            for (int i = qList.size() - 1; i >= 0 ; i--) {
+                Quotation q = qList.get(i);
+
+                System.out.printf("%d / %s / %s\n", q.getId(), q.getAuthor(), q.getContent());
             }
         }
     }
---
+++ src/main/java/com/ll/Quotation.java
@@ -0,0 +1,39 @@
+package com.ll;
+
+public class Quotation {
+    private int id;
+    private String content;
+    private String author;
+
+    // 생성자
+    Quotation(int id, String content, String author){
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    // id에 접근하는 메소드
+    int getId(){
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // content에 접근하는 메소드
+    String getContent(){
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    // author에 접근하는 메소드
+    String getAuthor(){
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
+
